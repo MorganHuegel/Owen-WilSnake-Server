890b85288aa4b5929fd4506416176cd10484b57a
MorganHuegel <mhue2910@gmail.com>
2019-08-27 11:40:12 -0400

    updated endpoint for saving a new game score.  it will return top three scores for that day, and that games rank for that day

--- routers/gameRouter.js
+++ routers/gameRouter.js
@@ -2,6 +2,15 @@ const express = require('express')
 const gameRouter = express.Router()
 const { knex } = require('../database/connectToDb')
 
+const { validateTokenMiddleware } = require('../authentication/jwt')
+
+/* DELETE THIS BEFORE PRODUCTION !!!!!!!!!!!!!!!! */
+gameRouter.get('/', async (req, res, next) => {
+  const allGames = await knex.select().from('games')
+  return res.status(418).json({ allGames })
+})
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+
 
 
 // GET ALL GAMES FOR SINGLE USER
@@ -13,6 +22,7 @@ gameRouter.get('/user/:userId', async (req, res, next) => {
       .from('games')
       .where({user_id: userId})
 
+    
     return res.json({games: allGames}).status(200)
   } 
   catch (err) {
@@ -73,15 +83,43 @@ gameRouter.get('/highScores/', async (req, res, next) => {
 
 
 // SAVE GAME DATA AFTER OWEN DIES
-gameRouter.post('/', async (req, res, next) => {
+gameRouter.post('/', validateTokenMiddleware, async (req, res, next) => {
   try {
-    const { score, numOfTouches, userId } = req.body
-    const newGameId = await knex
+    const userId = req.jwtPayload.id
+    const { score, numOfTouches } = req.body
+    const newGameSaved = await knex
       .returning('id')
       .insert({score, num_of_touches: numOfTouches, user_id: userId})
       .into('games')
 
-    return res.json({newGameId}).send(201)
+    if (!newGameSaved[0]) {
+      const err = new Error('Did not save game')
+      err.status = 400
+      return next(err)
+    }
+    const newGameId = newGameSaved[0]
+
+    const currentDate = new Date()
+    const todayTimestamp = `${currentDate.getUTCFullYear()}-${currentDate.getUTCMonth()}-${currentDate.getUTCDate()}`
+
+    const topScoresToday = await knex
+      .select('users.username', 'games.user_id', 'games.id', 'games.score', 'games.num_of_touches', 'games.played_on_ts')
+      .from('games')
+      .innerJoin('users', 'games.user_id', 'users.id')
+      .orderBy([{column: 'score', order: 'desc'}, {column: 'num_of_touches', order: 'desc'}])
+      .where('played_on_ts', '>=', todayTimestamp)
+      .andWhere('score', '>=', score)
+    
+    const topThreeScoresToday = topScoresToday.slice(0, 3)
+    const userRank = topScoresToday.findIndex(game => game.id === newGameId)
+    const inTopThree = (userRank <= 2) && (userRank >= 0)
+
+    return res.json({
+      topThreeScoresToday,
+      userRank,
+      userScore: topScoresToday[userRank],
+      inTopThree
+    })
   } 
   catch (err) {
     return next(err)
